ce8a3b5eb8101c716fdbdb60bf390c47884232cd
Barrett Bryson <bbryson@ncsu.edu>
2018-12-07 15:38:42 -0500

    Switched to structs instead of extra classes, started implementing the cycle flow methods

--- Pipeline.cpp
+++ Pipeline.cpp
@@ -11,8 +11,75 @@ Pipeline::Pipeline(unsigned long int width, unsigned long int iq_size, unsigned 
 	mWidth = width;
 	mARF = new int[67];
 
-	mROB = new ReorderBuffer(rob_size);
-	mIQ = new IssueQueue(iq_size);
-	mRMT = new RenameMapTable(67);
+	mROBsize = rob_size;
+
+	mROB = new rob_entry[rob_size];
+	mIQ = new iq_entry[iq_size];
+	mRMT = new rmt_entry[67];
+
+	mDEPR = new instr[width];
+    mRNPR = new instr[width];
+	mRRPR = new instr[width];
+	mDIPR = new instr[width];
+	mFU = new fu[width*5];
+	mWBPR = new instr[width*5];
+
+
+	mHead = 0;
+	mTail = 0;
 }
 
+//TODO handle superscalar...
+void Pipeline::retire(){
+	if (mROB[mHead].rdy) {
+		mHead = (mHead + 1) % mROBsize;
+	}
+}
+
+//handled with pointers?
+void Pipeline::writeback(){
+	for(int i = 0; i < mWidth*5; i++)
+	{
+		if (mWBPR[i].valid){
+			mROB[mWBPR[i].robID].rdy = true;
+			mWBPR[i].valid = false;
+		}
+	}
+}
+
+//This was being worked on
+void Pipeline::execute(){
+	for(int i = 0; i < mWidth*5; i++)
+	{
+		mFU[i].cyclesLeft--;
+		if(mFU[i].cyclesLeft == 0)
+		{
+
+		}
+	}
+
+}
+
+void Pipeline::issue() {
+
+}
+
+void Pipeline::dispatch() {
+
+}
+
+void Pipeline::regRead() {
+
+}
+
+void Pipeline::rename() {
+
+}
+
+void Pipeline::decode() {
+
+}
+
+void Pipeline::fetch() {
+
+}
--- Pipeline.h
+++ Pipeline.h
@@ -8,22 +8,80 @@
 #ifndef PIPELINE_H_
 #define PIPELINE_H_
 
-#include "ReorderBuffer.h"
-#include "IssueQueue.h"
-#include "RenameMapTable.h"
+typedef struct rob_entry{
+	int value;
+	unsigned int dst;
+	bool rdy;
+	bool exc;
+	bool miss;
+	unsigned long int pc;
+};
+
+typedef struct iq_entry{
+	bool valid;
+	unsigned int dstTag;
+	bool rs1Rdy;
+	bool rs1ROB;
+	int rs1Val;
+	bool rs2Rdy;
+	bool rs2ROB;
+	int rs2Val;
+};
+
+typedef struct rmt_entry {
+	bool valid;
+	unsigned int tag;
+};
+
+
+typedef struct instr {
+	bool valid;
+	unsigned long int pc;
+	unsigned int op_type;
+	int rd;
+	unsigned int r1;
+	unsigned int r2;
+	unsigned int robID;
+};
+
+typedef struct fu {
+	bool inUse;
+	unsigned int cyclesLeft;
+	instr fuInstr;
+};
 
 
 class Pipeline {
 public:
 	Pipeline(unsigned long int width, unsigned long int iq_size, unsigned long int rob_size);
+	void retire();
+	void writeback();
+	void execute();
+	void issue();
+	void dispatch();
+	void regRead();
+	void rename();
+	void decode();
+	void fetch();
 
 private:
 	unsigned long int mWidth;
 	int* mARF;
-	RenameMapTable mRMT;
-	ReorderBuffer mROB;
-	IssueQueue mIQ;
+	rob_entry* mROB;
+	unsigned int mROBsize;
+	unsigned int mHead;
+	unsigned int mTail;
+	iq_entry* mIQ;
+	rmt_entry* mRMT;
 
+
+	//Pipeline Registers
+	instr* mDEPR;
+	instr* mRNPR;
+	instr* mRRPR;
+	instr* mDIPR;
+	fu* mFU;
+	instr* mWBPR;
 };
 
 #endif /* PIPELINE_H_ */
